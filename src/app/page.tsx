1b46faad7b2d00a60c1847b42fa8942f63c81b0a
alex103040 <alexandre.pir.silva@gmail.com>
2024-08-11 17:31:18 +0100

    menu section start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Hero from "@/components/Hero";
+import MenuSection from "@/components/MenuSection";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <MenuSection />
+    </>
+  );
 }
---
+++ src/components/MenuSection.tsx
@@ -0,0 +1,31 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const MenuSection = () => {
+  return (
+    <>
+      <h2 className="scroll-m-20 border-b w-[80%] text-3xl font-semibold tracking-tight first:mt-0 text-center p-6 max-w-[1400px] mx-auto">
+        Menu
+      </h2>
+      {/* <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card> */}
+    </>
+  );
+};
+export default MenuSection;
