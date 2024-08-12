99ba0079779f5895723afe4d40ca59a55e5a8da7
alex103040 <alexandre.pir.silva@gmail.com>
2024-08-12 22:21:40 +0100

    menu

--- src/components/MenuSection.tsx
+++ src/components/MenuSection.tsx
@@ -1,6 +1,7 @@
 import { pizzas } from "../data/db";
 import Image from "next/image";
 
+import { Button } from "./ui/button";
 import {
   Card,
   CardContent,
@@ -16,10 +17,10 @@ const MenuSection = () => {
       <h2 className="scroll-m-20 border-b w-[80%] text-3xl font-semibold tracking-tight first:mt-0 text-center p-6 max-w-[1400px] mx-auto">
         Menu
       </h2>
-      <div className="grid grid-cols-3 gap-8 p-6 py-12 w-[80%] mx-auto max-w-[1400px]">
+      <div className="grid grid-cols-4 gap-8 p-6 py-12 w-[80%] mx-auto max-w-[1400px]">
         {pizzas.map((pizza) => (
-          <Card key={pizza.id}>
-            <CardHeader>
+          <Card key={pizza.id} className="flex flex-col">
+            <CardHeader className="grow">
               <CardTitle>{pizza.name}</CardTitle>
               <CardDescription>{pizza.description}</CardDescription>
             </CardHeader>
@@ -29,12 +30,13 @@ const MenuSection = () => {
                   src={`/menu/pizzas/${pizza.image}`}
                   alt="pizza image"
                   fill
-                  className="object-cover"
+                  className="object-cover rounded-xl border"
                 />
               </div>
             </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
+            <CardFooter className="flex justify-between">
+              <p>${pizza.price}</p>
+              <Button>Add to order</Button>
             </CardFooter>
           </Card>
         ))}
