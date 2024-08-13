77b899779e8028809ed5d531bfa8613b3cf90a82
alex103040 <alexandre.pir.silva@gmail.com>
2024-08-13 12:44:02 +0100

    scroll fix

--- src/components/MenuSection.tsx
+++ src/components/MenuSection.tsx
@@ -16,7 +16,7 @@ const MenuSection = () => {
     <>
       <h2
         id="menu-section"
-        className="scroll-m-20 border-b w-[80%] text-3xl font-semibold tracking-tight first:mt-0 text-center p-6 max-w-[1400px] mx-auto"
+        className=" scroll-m-[4.4rem] border-b w-[80%] text-3xl font-semibold tracking-tight first:mt-0 text-center p-6 max-w-[1400px] mx-auto"
       >
         Menu
       </h2>
